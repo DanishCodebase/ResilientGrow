20082bc6c00684da734a69ab701abe147a25ab72
Danish <ammarsaifi5376@gmail.com>
2024-03-03 11:15:10 +0530

    fourth commit

--- public/js/hero.js
+++ public/js/hero.js
@@ -67,6 +67,7 @@ show();
 
 document.querySelector("#cours").addEventListener("click", function () {
   document.querySelector(".cpu").classList.toggle("d-none");
+  document.querySelector(".cpu").classList.remove("show");
 });
 
 document.querySelector("#redirect").addEventListener("click", function () {
